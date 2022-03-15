5d9055796980234d854ad6f1d8c5351dde5b6903
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-03-15 19:33:06 -0300

    imutability

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,21 @@
 import { useState } from 'react';
 
+// imutabilidade
+
+// - A imutabilidade é um conceito da computação que diz que o valor de uma variável não pode sofrer alteração;
+// - Outras linguagens, como a linguagem funcional, por exemplo, também fazem uso da imutabilidade;
+// - Veja o exemplo abaixo: No primeiro momento, temos uma variável na memória chamada `usuarios`, um vetor com 2 itens;
+// - O que nós normalmente faríamos para adicionar um novo elemento seria fazer um push dentro da variável usuários, porém isso fará com que a variável usuários seja alterada (sofra mutação);
+// - Para continuarmos com o conceito de imutablidade o correto seria criarmos uma nova variável e fazer um spread operator na variável anterior, e  em seguida adicionar o novo valor (linha 9);
+// - A ação acima é realizada toda a vez que utilizamos o setState do React: É sempre criado uma nova variável com o mesmo nome na memória porém com valor diferente, respeitando assim o conceito da imutabilidade;
+// - Portanto, toda vez que é chamado o setCounter no código abaixo significa que está sendo criada uma nova variável na memória, chamada counter, com o valor counter +1;
+
+// usuarios = ['diego3g', 'diegosf']
+
+// usuarios.push('rafacamarda')
+
+// novoUsuarios = [...usuarios, 'rafacamarda']
+
 export function Counter() {
   const [counter, setCounter] = useState(0);
 
